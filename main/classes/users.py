136634b35a8993499d5245a158ca4562fa987f00
Mark Henderson <mark.henderson1988@gmail.com>
2020-11-09 21:49:31 +0000

    Added tests for the user class

--- main/classes/users.py
+++ main/classes/users.py
@@ -1,4 +1,10 @@
 class Users:
     def __init__(self, aName):
+        self.name = str(aName)
+        self.password_list = {}
+
+    def change_name(self, aName):
         self.name = aName
 
+    def add_password(self, aWebsite, aPassword):
+        self.password_list[aWebsite] = aPassword
