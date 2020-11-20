9e20481ca697282873913f4fb4b4cfc4f2b0a03a
Mark Henderson <mark.henderson1988@gmail.com>
2020-11-20 12:45:04 +0000

    Changes to functionality

--- main/gui_main.py
+++ main/gui_main.py
@@ -7,7 +7,7 @@ user_list = []
 def create_main_gui():
     # Define the window's contents
     left_column = [[sg.Text("-Users-")],
-                   [sg.Listbox(values=[], key='-USER_NAMES_LIST-', size=(30, 6))],
+                   [sg.Listbox(values=[], key='-USER_NAMES_LIST-', size=(30, 6), enable_events=True)],
                    [sg.Button('Add', key='-NEW_USER_NAME_BUTTON-')],
                    [sg.Button('Edit', key='-EDIT_USER_NAME_BUTTON-')],
                    [sg.Button('Delete', key='-DELETE_USER_NAME_BUTTON-')]]
@@ -31,7 +31,6 @@ def create_main_gui():
             break
         if event == '-NEW_USER_NAME_BUTTON-':
             new_user_name(user_list)
-            names = [user.name for user in user_list]
-            window['-USER_NAMES_LIST-'].update(names)
+            window['-USER_NAMES_LIST-'].update(user.name for user in user_list)
     # Finish up by removing from the screen
     window.close()
